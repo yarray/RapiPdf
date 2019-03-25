e9022bc3ff2d496c0b7be53fefd08d8963ff1624
Mrin <mrin9@yahoo.com>
2019-03-24 23:43:12 -0700

    updated webpack config and documentation

--- docs/rapipdf-min.js
+++ docs/rapipdf-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1b96ee65b24db0f52c10865526591c56b0d920f282b1972bd7477050d231683
-size 2398585
+oid sha256:b677c9dc57bbb467ff3d9ecdd0d72ed8b895dee48db08330ba067e9ec9f8b4a9
+size 2398732
