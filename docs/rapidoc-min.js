01c259bfdab559873b91093c1d2c10413339dfe3
Mrin <mrin9@yahoo.com>
2019-03-24 20:08:55 -0700

    removed unused fonts

--- docs/rapidoc-min.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b393937374e08a5e542530d100ffebbb16e8770d24844eaa1ad8f4691b8b88d7
-size 478021
--- docs/rapipdf-min.js
+++ docs/rapipdf-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a23db2d26f1a3af4bf19eb041745e4a37ec6dea3bdc2be16b299fda32f28291d
-size 2775610
+oid sha256:d1b96ee65b24db0f52c10865526591c56b0d920f282b1972bd7477050d231683
+size 2398585
