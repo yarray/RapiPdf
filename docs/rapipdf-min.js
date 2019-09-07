ba2bd998e30c2dc7f795a353f891c12653e8ba4c
Mrin <mrin9@yahoo.com>
2019-09-07 14:19:49 -0700

    added localization feature

--- docs/rapipdf-min.js
+++ docs/rapipdf-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f4d944a0ec59e2ed5482a7073d73b332467654bc4d8d291f0dcfaddd4536764
-size 2415728
+oid sha256:3ca521f368dbaa32fc2993c38ee1033ce82463045df2856a3fa674a23a565022
+size 2416839
