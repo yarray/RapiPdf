7b393e6730a8a3b8977adf66c6ec429bd5dd9146
Mrin <mrin9@yahoo.com>
2019-09-28 15:27:54 -0700

    fixed type on path

--- docs/rapipdf-min.js
+++ docs/rapipdf-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c79b127fcb5b725041f9b50f42f0cff2101f688f948dedf94beb31f0e38d07b5
-size 2550533
+oid sha256:56431f899d2e5d44a3d3cd973e3a442c76b84d4f35f78e6888179b4e67d30bbe
+size 2550386
