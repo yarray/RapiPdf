6ce35d3d2bb84711204fcb1b28d85e8e685d4ca6
Mrin <mrin9@yahoo.com>
2019-10-04 21:15:58 -0700

    feat: new schema rendering style added

--- docs/rapipdf-min.js
+++ docs/rapipdf-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56431f899d2e5d44a3d3cd973e3a442c76b84d4f35f78e6888179b4e67d30bbe
-size 2550386
+oid sha256:d72bea242a5147d5b338a9935afe1eee6bbfb6d0732b90bb644242877fd804e3
+size 2552269
---
+++ src/vfs_fonts.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:71176374e8a39929823a4c5a9f56d8a19760f35b569c37dea8e4d78da774eb34
+size 920907
