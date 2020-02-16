a5e117eb8a03c3127543ece17ead345aed6c040d
Mrin <mrin9@yahoo.com>
2020-02-15 22:36:24 -0800

    generated build for 2.1.1

--- docs/rapipdf-min.js
+++ docs/rapipdf-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d98f8de43c3b5ffe4acb7636372c6c25ab1bad07f2fcfb139ea91230b55f417
-size 2552953
+oid sha256:53e1e7a4127405b375dd05c1405f75adcf0bce039afe31e1bde9fece70ba8f27
+size 2596016
