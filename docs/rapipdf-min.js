10437686c95ca6bd1f744acf8574788420422aab
yarray <08to09@gmail.com>
2022-01-10 20:58:34 +0800

    clean up
    Former-commit-id: a60f533090ca30c9dae9c3a2b3c5d230db583df9

--- docs/rapipdf-min.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:862bf330cedccd3e69ab175bf73ce9c9f7e661051d88b3b544fe3654c0ee6afb
-size 113512580
--- src/vfs_fonts.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b7f0832480986cfe2db4b5eef6cfa3295ba7f3e4b23f6e21a2810b015a07596e
-size 111841843
